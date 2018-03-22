40e5e2cce11515725f2613508b18f778decd5f95
Robert Dodson <rad@robdodson.net>
2018-03-22 17:43:59 -0400

    Initial Check in

--- FrogCalc/ViewController.swift
+++ FrogCalc/ViewController.swift
@@ -8,20 +8,120 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+enum Operation
+{
+    case Add
+    case Sub
+    case Div
+    case Mult
+    case None
+}
 
-    override func viewDidLoad() {
+class ViewController: NSViewController
+{
+    var tmp: Float = 0.0
+    var needclear:Bool = false
+    var lastop:Operation = .None
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    func append(val:String)
+    {
+        if (needclear)
+        {
+            register.stringValue = ""
+            needclear = false
+        }
+        
+        register.stringValue = register.stringValue.appending(val)
+    }
+   
+    
+    func calc()
+    {
+        switch lastop
+        {
+            case .Add:
+            tmp = tmp + register.floatValue
+            
+            case .Sub:
+            tmp = tmp - register.floatValue
+            
+            case .Mult:
+            tmp = tmp * register.floatValue
+            
+            case .Div:
+            tmp = tmp / register.floatValue
+            
+            case .None:
+                return
         }
     }
+    
+    
+    func accum(op:Operation)
+    {
+        if (lastop == .None)
+        {
+            tmp = register.floatValue
+        }
+        else
+        {
+            calc()
+        }
+        
+        register.stringValue = tmp.description;
+        lastop = op
+        
+        needclear = true
+    }
 
+    
+    //
+    //  Handle for number textfield
+    //
+    @IBOutlet weak var register: NSTextField!
 
+    
+    //
+    // Button actions
+    //
+    @IBAction func sevenAction(_ sender: NSButton) { append(val:"7") }
+    @IBAction func eightAction(_ sender: NSButton) { append(val:"8") }
+    @IBAction func nineAction(_ sender: NSButton)  { append(val:"9") }
+    @IBAction func sixAction(_ sender: NSButton)   { append(val:"6") }
+    @IBAction func fiveAction(_ sender: NSButton)  { append(val:"5") }
+    @IBAction func fourAction(_ sender: NSButton)  { append(val:"4") }
+    @IBAction func threeAction(_ sender: NSButton) { append(val:"3") }
+    @IBAction func twoAction(_ sender: NSButton)   { append(val:"2") }
+    @IBAction func oneAction(_ sender: NSButton)   { append(val:"1") }
+    @IBAction func zeroAction(_ sender: NSButton)  { append(val:"0") }
+    @IBAction func dotAction(_ sender: NSButton)   { append(val:".") }
+    
+    @IBAction func divAction(_ sender: NSButton)   { accum(op:.Div)  }
+    @IBAction func multAction(_ sender: NSButton)  { accum(op:.Mult) }
+    @IBAction func minusAction(_ sender: NSButton) { accum(op:.Sub)  }
+    @IBAction func addAction(_ sender: NSButton)   { accum(op:.Add)  }
+    
+    @IBAction func equalsAction(_ sender: NSButton)
+    {
+        calc()
+        register.stringValue = tmp.description;
+        lastop = .None
+    }
+    
+    @IBAction func clearAction(_ sender: NSButton)
+    {
+        register.stringValue = ""
+        tmp = 0.0
+        lastop = .None
+        needclear = false
+    }
+    
 }
 
