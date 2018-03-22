09c38ced03f69a6846ebac9972795096c0999b43
Robert Dodson <rad@robdodson.net>
2018-03-22 10:24:30 -0400

    Initial Commit

---
+++ FrogCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FrogCalc
+//
+//  Created by Robert Dodson on 3/22/18.
+//  Copyright © 2018 Robert Dodson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
